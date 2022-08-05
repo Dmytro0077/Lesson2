4aa5615497d8d0c7a850f4bd9df3c3f822c3d11a
Dmytro0077 <kaktusgren@gmail.com>
2022-08-05 15:48:24 +0300

    addHomeworkExtra

---
+++ Lesson2/Lesson2/HomeworkExtra.cs
@@ -0,0 +1,14 @@
+﻿class Program2
+{
+    public void HomeworkExtra()
+    {
+        DateTime date = DateTime.Today;
+        int daysInYear = DateTime.IsLeapYear(date.Year) ? 366 : 365;
+        int daysLeftInYear = daysInYear - date.DayOfYear;
+        int daysRightInYear = daysInYear - daysLeftInYear;
+       
+        Console.WriteLine($"Цього року в році: {daysInYear} днів");
+        Console.WriteLine($"До нового року залишилось: {daysLeftInYear}");
+        Console.WriteLine($"Від нового року минуло: {daysRightInYear}");
+    }
+}
